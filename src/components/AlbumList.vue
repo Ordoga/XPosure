8ef30034bf7d59efcb5c4ab9eab7cf2074ce8698
Ordoga <ordogga@gmail.com>
2024-08-26 19:05:01 +0300

    File Structure and basic services added

---
+++ src/components/AlbumList.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+    },
+    created() {
+    },
+    components: {
+    }
+}
+</script>
+
+<style lang='scss' scoped>
+
+</style>
